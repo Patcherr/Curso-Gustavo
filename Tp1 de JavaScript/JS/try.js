272d8ccf0e9c3de6b0fc56363cb9139eb2c58c37
Patcherr <137843869+Patcherr@users.noreply.github.com>
2023-06-29 21:12:25 -0300

    Update try.js

--- Tp1 de JavaScript/JS/try.js
+++ Tp1 de JavaScript/JS/try.js
@@ -1,26 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const display = document.querySelector(".display");
-    const buttons = document.querySelectorAll(".number, .operator, .decimal");
-    const calculateBtn = document.getElementById("calculate");
-    const clearBtn = document.getElementById("clear");
-  
-    buttons.forEach((button) => {
-      button.addEventListener("click", function() {
-        const value = button.textContent;
-        display.value += value;
-      });
-    });
-  
-    calculateBtn.addEventListener("click", function() {
-      try {
-        display.value = eval(display.value);
-      } catch (error) {
-        display.value = "Error";
-      }
-    });
-  
-    clearBtn.addEventListener("click", function() {
-      display.value = "";
-    });
+window.addEventListener("DOMContentLoaded", function() {
+  const display = document.getElementById("displayvalue");
+  const buttons = Array.from(document.querySelectorAll(".number,.operator, .decimal"));
+  const calculateBtn = document.getElementById("calculate");
+  const clearBtn = document.getElementById("clear");
+
+  function eventButtom(event){
+    const buttomValue = event.target.innerText
+    display.innerHTML += buttomValue
+  }
+
+
+  buttons.forEach(function(button)  {
+    button.addEventListener("click", eventButtom) 
+          
   });
-  
+
+  calculateBtn.addEventListener("click", function() {
+    try {
+      display.innerHTML = eval(display.innerHTML);
+    } catch (error) {
+      display.innerHTML = "Error";
+    }
+  });
+
+  clearBtn.addEventListener("click", function() {
+    display.innerHTML = "";
+  });
+});
