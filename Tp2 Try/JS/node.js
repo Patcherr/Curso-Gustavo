0605b107db35f381a43785d741e70c1f8e4da540
BlueLuscious <lucio.200152@gmail.com>
2023-06-30 02:03:18 -0300

    Update node.js

--- Tp2 Try/JS/node.js
+++ Tp2 Try/JS/node.js
@@ -21,45 +21,53 @@ formulario.addEventListener('submit', function(event) {
   alert(`Valores ingresados:\n\nNombre: ${nombre}\nApellido: ${apellido}\nDNI: ${dni}\nFecha de Nacimiento: ${fechaNacimiento}\nGénero: ${genero}\nConocimientos: ${conocimientos.join(', ')}\nBiografía: ${bio}`);
 }); */
 
+window.addEventListener('DOMContentLoaded', function () {
 
-const formulario = document.getElementById('Formulario');
-const enviar = document.getElementById('enviar');
+  const nombre = document.getElementById('nombre').value
+  const apellido = document.getElementById('apellido').value
+  const dni = document.getElementById('dni').value
+  const fechaNacimiento = document.getElementById('fechaNacimiento').value
+  const genero = document.querySelector('input[name="genero"]:checked').value
+  const conocimientos = Array.from(document.querySelectorAll('input[name="conocimientos"]:checked')).map(input => input.value)
+  const bio = document.getElementById('bio').value
 
-enviar.addEventListener('click', function (event) {
-  event.preventDefault();
+  const enviar = document.getElementById('enviar');
+  const formulario = document.getElementById('Formulario');
 
-  const nombre = encodeURIComponent(document.getElementById('nombre').value);
-  const apellido = encodeURIComponent(document.getElementById('apellido').value);
-  const dni = encodeURIComponent(document.getElementById('dni').value);
-  const fechaNacimiento = encodeURIComponent(document.getElementById('fechaNacimiento').value);
-  const genero = encodeURIComponent(document.querySelector('input[name="genero"]:checked').value);
-  const conocimientos = Array.from(document.querySelectorAll('input[name="conocimientos"]:checked')).map(input => input.value);
-  const bio = encodeURIComponent(document.getElementById('bio').value);
-  formulario.submit();
+  enviar.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    formulario.submit();
+  });
+
+
+})
+
+window.addEventListener('DOMContentLoaded', function () {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const nombre = urlParams.get('nombre');
+  const apellido = urlParams.get('apellido');
+  const dni = urlParams.get('dni');
+  const fechaNacimiento = urlParams.get('fechaNacimiento');
+  const genero = urlParams.get('genero');
+  const conocimientos = urlParams.get('conocimientos');
+  const bio = urlParams.get('bio');
+
+  // Ahora puedes hacer lo que desees con los datos del formulario en otro_archivo.html
+  // Por ejemplo, puedes mostrar los valores en la página:
+  document.getElementById('nombreResultado').innerHTML = nombre;
+  document.getElementById('apellidoResultado').innerHTML = apellido;
+  document.getElementById('dniResultado').innerHTML = dni;
+  document.getElementById('fechaNacimientoResultado').innerHTML = fechaNacimiento;
+  document.getElementById('generoResultado').innerHTML = genero;
+  document.getElementById('conocimientosResultado').innerHTML = conocimientos;
+  document.getElementById('bioResultado').innerHTML = bio
 
 
 });
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
 
-const nombre = urlParams.get('nombre');
-const apellido = urlParams.get('apellido');
-const dni = urlParams.get('dni');
-const fechaNacimiento = urlParams.get('fechaNacimiento');
-const genero = urlParams.get('genero');
-const conocimientos = urlParams.get('conocimientos');
-const bio = urlParams.get('bio');
-
-// Ahora puedes hacer lo que desees con los datos del formulario en otro_archivo.html
-// Por ejemplo, puedes mostrar los valores en la página:
-document.getElementById('nombreResultado').innerHTML = nombre;
-document.getElementById('apellidoResultado').innerHTML = apellido;
-document.getElementById('dniResultado').innerHTML = dni;
-document.getElementById('fechaNacimientoResultado').innerHTML = fechaNacimiento;
-document.getElementById('generoResultado').innerHTML = genero;
-document.getElementById('conocimientosResultado').innerHTML = conocimientos;
-document.getElementById('bioResultado').innerHTML = bio
 
 
 
