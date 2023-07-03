ba0c96b38daf1789e20ddd17ae4b2bc8be5a6786
Patcherr <137843869+Patcherr@users.noreply.github.com>
2023-07-03 01:29:21 -0300

    Create 02_Operadores(1).py

---
+++ Python/02_Operadores(1).py
@@ -0,0 +1,55 @@
+### OPERADORES ARITMETICOS ###
+""""
+# '='
+numero = 5
+# '+='
+numero += 3 
+# '-='
+numero -= 7
+# '/='
+numero /= 2
+# '%='
+numero %= 10
+# '//='
+numero //= 20
+# '**='
+numero **= 40
+# '&='
+numero &= 4
+# '|='
+numero |= 2
+# '^='
+numero ^= 4
+# '>>='
+numero >>= 3
+# '<<='
+numero <<= 5
+"""
+## EJEMPLOS ##
+
+print(6 + 4)
+print(6 - 4)
+print(6 * 4)
+print(6 / 4)
+
+# Modulos
+# Modulo es que se efectua una división y lo que devuelve, es el resto que queda de la misma operación
+print(10 % 2)
+
+# Es una división que aproxima el resultado a un entero
+print(10 // 3)
+
+#Calculo de un exponente (Ej: 2 elevado a las 3)
+print(2 ** 3)
+
+
+print("Hola" + "Python")
+
+# ¡Cuidado esto no se puede hacer!
+print("Hola " + 5)
+
+# Esto si se peude hacer
+print("Hola " + str(5))
+
+#Esto multiplica una cadena de texto
+print("hola " * 5)
