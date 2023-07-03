3689959e39fc0c8997e6d9f14f7692dfe6e1daaf
Patcherr <137843869+Patcherr@users.noreply.github.com>
2023-07-03 01:29:24 -0300

    Create 02_Operadores(2).py

---
+++ Python/02_Operadores(2).py
@@ -0,0 +1,21 @@
+### OPERADORES COMPARATIVOS ###
+"""""
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+"""
+
+# Se comprueba una Ordenación Alfabética por ASCII
+# ¡NO SE COMPRUEBA LOS CARACTERES!
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python")
+print("Hola" <="Python")
+print("Hola" =="Python")
+print("Hola" != "Python")
+
+# Cuenta Caracteres
+print(len("Hola" > "Python"))  
