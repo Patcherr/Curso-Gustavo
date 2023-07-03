074de3fac64ca87f2c8242c501522bf249bbec3f
Patcherr <137843869+Patcherr@users.noreply.github.com>
2023-07-03 01:29:16 -0300

    Create 00_Introducción.py

---
+++ Python/00_Introducción.py
@@ -0,0 +1,25 @@
+### INTRODUCCION ###
+
+#Esto es un comentario
+"Otro tipo de comentario"
+'Otro comentario mas'
+#Esto es un Print(Impresion en pantalla)
+"""
+Esto Es
+un comentario
+de varias
+lineas
+"""
+print("Hola Mundo")
+print('Hola Mundo')
+
+#Consultar el tipo de dato
+print(type("Hola python")) # Tipo 'str'(string)
+print(type(5)) # Tipo 'Int' (Entero)
+print(type(1.5)) # Tipo 'float' (Numericos)
+print(type(True)) # Tipo 'bool' (Decisión)
+print(type(1 + 3j)) # Tipo 'complex' (Numeros Complejos)
+
+#Error
+print(type(print("Mi cadena de texto"))) #Tipo 'NoneType'
+
