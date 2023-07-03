a5d9cb55aa78131b90daebd0fba809177db7aed9
Patcherr <137843869+Patcherr@users.noreply.github.com>
2023-07-03 01:29:26 -0300

    Create 02_Operadores(3).py

---
+++ Python/02_Operadores(3).py
@@ -0,0 +1,13 @@
+### OPERADORES LOGICOS ###
+
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python" )
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" < "Python" )
+print(3 < 4 and "Hola" > "Python")
+
+# ¡CUIDADO ASI NO SE ESCRIBE!
+#print(3 > 4 not "Hola" > "Python")
+
+# ASI SE ESCRIBE
+print(not(3 > 4))
